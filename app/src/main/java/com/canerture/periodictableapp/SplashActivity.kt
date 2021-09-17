9101a73f33d421d27f4f8e8ccbadc053128d0b9d
Caner Türe <cnrture@gmail.com>
2021-09-18 00:03:44 +0300

    Intent animation added

--- app/src/main/java/com/canerture/periodictableapp/SplashActivity.kt
+++ app/src/main/java/com/canerture/periodictableapp/SplashActivity.kt
@@ -3,11 +3,8 @@ package com.canerture.periodictableapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.canerture.periodictableapp.databinding.ActivitySplashBinding
-
-import com.jaredrummler.android.widget.AnimatedSvgView
 
 class SplashActivity : AppCompatActivity() {
 
@@ -27,6 +24,7 @@ class SplashActivity : AppCompatActivity() {
 
                     val intent = Intent(this@SplashActivity, MainActivity::class.java)
                     startActivity(intent)
+                    overridePendingTransition(R.anim.slide_out, R.anim.slide_in);
                 }catch (e : Exception){
                     e.printStackTrace()
                 }
